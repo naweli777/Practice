97c1c5f1d0b5dfef93b132c971fff7754645270e
naweli7 <naweli.vinktek@gmail.com>
2020-05-04 10:25:10 +0530

    practice

---
+++ src/Mainpage/Mainpage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Mainpage.css';
+
+
+const Mainpage = (props) => {
+    return (
+
+    
+    <div >
+       
+        <img src = {require('../Imagess/meeee.jpg')} className="mainImage"/>
+        <p className="about">Hey! This is Naweli Verma</p>
+
+
+        </div>
+    );
+};
+export default Mainpage;
